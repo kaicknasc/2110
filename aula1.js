0f4c1682d3c714ec20799bb0a30a9680cdfe0df6
Wellington da Silva <nenem12@icloud.com>
2024-10-21 21:33:30 -0300

    2110

---
+++ aula1.js
@@ -0,0 +1,15 @@
+// let messagedisplay = ("Este texto é um exemplo para podermos praticar os exercícios");
+
+// console.log(messagedisplay);
+
+// messagedisplay = ('Alterando minha variável com a função let');
+
+// console.log(messagedisplay);
+
+const messagedisplay = ("Está mensagem é um teste.");
+
+// console.log(messagedisplay);
+
+messagedisplay = ("Tentando alterar variável constante");
+
+console.log(messagedisplay);
---
+++ aula2.js
@@ -0,0 +1,27 @@
+// OPERADORES JAVASCRIPT
+
+// + = ADIÇÃO
+// - = SUBTRAÇÃO
+// / = DIVISÃO
+// * = MULTIPLICAÇÃO
+
+const resultado = 7 + 3;
+console.log(resultado)
+
+const resultadoSubtracao = 9 - 5;
+console.log(resultadoSubtracao)
+
+const resultadoDivisao = 25 / 2;
+console.log(resultadoDivisao)
+
+const resultadoMultiplicacao = 5 * 85;
+console.log(resultadoMultiplicacao)
+
+
+// Podemos usar esses operadores em outras situações, como incrementar uma frase por exemplo.
+
+const text1 = 'Olá, meu nome é Dom Bosco ';
+const text2 = 'Eu sou aluno da Obra Social Dom Bosco';
+
+const textofinal = text1 + text2;
+console.log(textofinal);
